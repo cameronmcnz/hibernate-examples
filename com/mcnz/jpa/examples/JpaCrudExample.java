eb9827499cf6948a955d7317afb326ad18d4f2a0
Cameron McKenzie <38986973+cameronmcnz@users.noreply.github.com>
2020-05-27 18:19:49 -0400

    Update JpaCrudExample.java

--- com/mcnz/jpa/examples/JpaCrudExample.java
+++ com/mcnz/jpa/examples/JpaCrudExample.java
@@ -1,28 +1,78 @@
 package com.mcnz.jpa.examples;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 
 public class JpaCrudExample {
 
-	public static void main(String[] args) {
+
+	public static void main(String args[]) {
+	
+		MyDatabaseWizard.main(null);
+		JpaCrudExample.createRecord();
+		JpaCrudExample.readRecord();
+		JpaCrudExample.updateRecord();
+		JpaCrudExample.deleteRecord();
 		
-		//MyDatabaseWizard.addEntity(Player.class);
-		//MyDatabaseWizard.createDatabase();
-		
+	
+	}
+	
+	public static void createRecord() {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-tutorial");
 		EntityManager entityManager = emf.createEntityManager();
 		
 		entityManager.getTransaction().begin();
 		
-		Player player = new Player();
-		player.setName("Bart");
-		player.setPassword("kowabunga");
-		entityManager.persist(player);
+		Player player0 = new Player("Bart", "kowabunga");
+		Player player1 = new Player("Homer", "doh!");
+		Player player2 = new Player("Ralph", "choo-choo");
+
+
+		entityManager.persist(player0);
+		entityManager.persist(player1);
+		entityManager.persist(player2);
 		
 		entityManager.getTransaction().commit();
 		
-
 	}
+	
+	public static void readRecord() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-tutorial");
+		EntityManager entityManager = emf.createEntityManager();
+		
+		entityManager.getTransaction().begin();
+		
+		Player player = entityManager.find(Player.class, Long.valueOf(2));
+		System.out.println("Password is: " + player.getPassword());
+		
+		entityManager.getTransaction().commit();
+		
+	}
+	
+	public static void updateRecord() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-tutorial");
+		EntityManager entityManager = emf.createEntityManager();
+		
+		entityManager.getTransaction().begin();
+		
+		Player player = entityManager.find(Player.class, Long.valueOf(2));
+		player.setPassword("whyyou!!!");
+		
+		entityManager.getTransaction().commit();
+		
+	}
+	
+	public static void deleteRecord() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-tutorial");
+		EntityManager entityManager = emf.createEntityManager();
+		
+		entityManager.getTransaction().begin();
+		
+		Player player = entityManager.find(Player.class, Long.valueOf(2));
+		entityManager.remove(player);
+		
+		entityManager.getTransaction().commit();
+		
+	}
+	
+	
 }
